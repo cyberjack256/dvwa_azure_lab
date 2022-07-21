0572a13a00b4c9dc54bbe5b52927b92da66f3afe
Jack Turner <97242526+cyberjack256@users.noreply.github.com>
2022-07-21 11:57:42 -0400

    Update Onboarding_github.sh

--- Cloning a Github Repo/Onboarding_github.sh
+++ Cloning a Github Repo/Onboarding_github.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-name="Jack" #Your Name (e.g., "Jack")
-email="cyberjack256@gmail.com" #Your Email (e.g., "Jack@gmail.com")
-oauthtoken="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" # Your Access Token (e.g., "fjklajfldjslkjfa;ldkfjas8937487325")
-username="cyberjack256" # Your Username (e.g.,"cyberjack256")
+name="name" #Your Name (e.g., "Jack")
+email="email@email.com" #Your Email (e.g., "Jack@gmail.com")
+oauthtoken="FakEtoKen283947893743" # Your Access Token (e.g., "fjklajfldjslkjfa;ldkfjas8937487325")
+username="username" # Your Username (e.g.,"cyberjack256")
 repo="dvwa_azure_lab" # Your Repo (e.g, "dvwa_azure_lab")
-
-
-#gitconfig [--global || --local]
-git config --local user.name "$name"
-git config --local user.email "$email"
-
-git config --local --list
-git config --global --list
 
 git clone https://$oauthtoken@github.com/$username/$repo.git
